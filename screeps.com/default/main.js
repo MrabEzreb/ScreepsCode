01eed56e34963d30bed8ac80baf23fdca4560bd9
Mrab Ezreb <zerbebram@gmail.com>
2016-12-30 17:07:24 -0500

    added profiler

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -2,17 +2,21 @@ var memClear = require("sys.memclear")
 var roomMain = require("room.main")
 var roleLoop = require("role.loop")
 var spawnerLoop = require("spawner.loop")
+const profiler = require("screeps-profiler")
 
 var room = "W38S75"
 
+profiler.enable()
 module.exports.loop = function() {
-    if(room != null) {
-        Game.rooms[room].find(FIND_MY_SPAWNS)[0].memory.buildingM = "Building";
-        Game.rooms[room].find(FIND_MY_SPAWNS)[0].memory.repairingM = "Repairing";
-        require("prototype")()
-        memClear()
-        roomMain(room)
-        roleLoop.runAll()
-        spawnerLoop.pre(Game.rooms[room].find(FIND_MY_SPAWNS)[0].name)
-    }
+    profiler.wrap(function() {
+        if(room != null) {
+            Game.rooms[room].find(FIND_MY_SPAWNS)[0].memory.buildingM = "Building";
+            Game.rooms[room].find(FIND_MY_SPAWNS)[0].memory.repairingM = "Repairing";
+            require("prototype")()
+            memClear()
+            roomMain(room)
+            roleLoop.runAll()
+            spawnerLoop.pre(Game.rooms[room].find(FIND_MY_SPAWNS)[0].name)
+        }
+    })
 }
